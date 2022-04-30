2b9f05b2d9fdde752c6406b3a7a6f3b69511cfa4
paulisaenz <sae21002@byui.edu>
2022-04-30 12:30:55 -0300

    first commit

---
+++ saenz_tictactoe.py
@@ -0,0 +1,32 @@
+
+# File 02 Ponder and Prove Assignment: Tic-Tac-Toe
+# Author: Paula Sáenz
+
+def main():
+    pass
+    #print("1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9")
+    #x1 = input("x's turn to choose a square (1-9): ")
+    #sure = input(f"Are you sure you want to put the x in {x1}? ")
+
+    #if sure.lower() in "yes":
+
+class Game:
+    def __init__(self):
+        self.turn = 1
+
+    def game_loop():
+        pass
+
+    def display_board():
+        pass
+
+    def user_imput():
+        pass
+
+    def update_board():
+        pass
+
+    def check_results():
+        pass
+
+main()
